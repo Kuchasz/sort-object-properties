525849114833ef3293ca48fe93376c03c4c1015a
Rafal Pysz <kuchasz1@o2.pl>
2017-02-05 12:27:30 +0100

    extended api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export default function sortObject(obj) {
-    return Object.keys(obj).sort().reduce((r, key)=>({...r, ...{[key]: obj[key]}}), {});
+export default function sort(obj, compareFunction) {
+    const wrappedFunction = compareFunction ? (a, b) => (compareFunction({key: a, value: obj[a]}, {key: b, value: obj[b]})) : undefined;
+    return Object.keys(obj).sort(wrappedFunction).reduce((r, key)=>({...r, ...{[key]: obj[key]}}), {});
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,21 +1,55 @@
-const _sortedObject = {
-    a: 'value',
-    g: 'value',
-    j: 'value',
-    kolm: 'value',
-    zet: 'value'
+import sort from '../src/index';
+
+const _sortedAscendingKeys = ['a', 'b', 'c', 'd', 'e'];
+
+const _sortedDescendingKeys = ['e', 'd', 'c', 'b', 'a'];
+
+const _unsortedKeysObject = {
+    d: 'value1',
+    c: 'value2',
+    e: 'value3',
+    b: 'value4',
+    a: 'value5'
 };
 
-const _unsortedObject = {
-    kolm: 'value',
-    g: 'value',
-    zet: 'value',
-    j: 'value',
-    a: 'value'
+const _sortedAscendingValues = ['a', 'b', 'c', 'd', 'e'];
+
+const _sortedDescendingValues = ['e', 'd', 'c', 'b', 'a'];
+
+const _unsortedValuesObject = {
+    key1: 'd',
+    key2: 'a',
+    key3: 'e',
+    key4: 'b',
+    key5: 'c'
 };
 
 describe('sort-object-properties', () => {
-    test('object properties sorted properly',()=>{
-        expect(2).toEqual(2);
+    describe('by key', () => {
+
+        test('ascending',() => {
+            const _sortedObject = sort(_unsortedKeysObject);
+            expect(Object.keys(_sortedObject)).toEqual(_sortedAscendingKeys);
+        });
+
+        test('descending',() => {
+            const _sortedObject = sort(_unsortedKeysObject, ({key: aKey}, {key: bKey}) => (aKey <= bKey));
+            expect(Object.keys(_sortedObject)).toEqual(_sortedDescendingKeys);
+        });
+
+    });
+
+    describe('by simple value', () => {
+
+        test('ascending', () => {
+            const _sortedObject = sort(_unsortedValuesObject, ({value: aVal}, {value: bVal}) => (aVal >= bVal));
+            expect(Object.values(_sortedObject)).toEqual(_sortedAscendingValues);
+        });
+
+        test('descending', () => {
+            const _sortedObject = sort(_unsortedValuesObject, ({value: aVal}, {value: bVal}) => (aVal <= bVal));
+            expect(Object.values(_sortedObject)).toEqual(_sortedDescendingValues);
+        });
+
     });
 });
